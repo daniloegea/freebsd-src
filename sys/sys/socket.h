7ef71401ec0b0dde9a6c2a85328db5fd15da8ec1
Danilo Egea Gondolfo <danilo@FreeBSD.org>
2023-09-30 16:49:39 +0100

    vsock: add support for Virtio sockets and AF_VSOCK
    WIP!

--- sys/sys/socket.h
+++ sys/sys/socket.h
@@ -268,7 +268,8 @@ struct accept_filter_arg {
 #define	AF_INET6_SDP	42		/* OFED Socket Direct Protocol ipv6 */
 #define	AF_HYPERV	43		/* HyperV sockets */
 #define	AF_DIVERT	44		/* divert(4) */
-#define	AF_MAX		44
+#define AF_VSOCK	46		/* Virtio VSOCK */
+#define	AF_MAX		46
 /*
  * When allocating a new AF_ constant, please only allocate
  * even numbered constants for FreeBSD until 134 as odd numbered AF_
@@ -394,6 +395,7 @@ struct sockproto {
 #define	PF_INET_SDP	AF_INET_SDP
 #define	PF_INET6_SDP	AF_INET6_SDP
 #define	PF_DIVERT	AF_DIVERT
+#define	PF_VSOCK	AF_VSOCK
 
 #define	PF_MAX		AF_MAX
 
